822e604cf7bdcb6e13897f66671909afc9376cb7
PaolaIzaguirre <eviizaguirre@yahoo.com>
2019-02-05 20:11:17 -0600

    Login
    Login Del Sistema, Y Clase Css Utilizada Para Los Estilos

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/log.css">
+</head>
+<body>
+	<h1>Laboratorio Clínico Emanuel</h1>
+
+<div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <label>Correo:</label>
+      <input type="text" class="form-control" />
+      <label>Contraseña:</label>
+      <input type="password" class="form-control" />
+      <button class="btn btn-primary">Iniciar Sesion</button>
+      <br><br><br>
+      <a href="#">Olvidé mi contraseña</a>
+      <br>
+    </form>
+  </div>
+  <button class="btn btn-primary reg">Registrarse</button>
+</div>
+
+
+</body>
+</html>
