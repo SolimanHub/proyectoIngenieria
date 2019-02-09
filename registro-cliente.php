8baca0e2e46d7d2c33d4ddabe526720d4035f854
PaolaIzaguirre <eviizaguirre@yahoo.com>
2019-02-08 22:44:36 -0600

    Registro Cliente
    Se Realizo Una Modificacion En El Fondo Del Login Y Del Registro De Los
    Modulos, Ademas Se Añadio El Registro De Cliente Desde El Cual Se Podra
    Registra El Cliente En La PAgina Del Laboratorio

--- registro-cliente.php
+++ registro-cliente.php
@@ -12,15 +12,15 @@
 <div class="login-page">
   <div class="form">
     <form class="login-form">
-      <input type="text" class="form-control" placeholder="Correo" />
+      <input type="text" class="form-control" placeholder="Nombre" />
+      <input type="text" class="form-control" placeholder="Apellido" />
+      <input type="email" class="form-control" placeholder="Correo" />
       <input type="password" class="form-control" placeholder="Contraseña" />
-      <button class="btn btn-primary">Iniciar Sesion</button>
-      <br><br><br>
-      <a href="#">Olvidé mi contraseña</a>
-      <br>
+      <input type="password" class="form-control" placeholder="Repetir Contraseña" />
+      <button class="btn btn-primary">Registrarse</button>
+      
     </form>
   </div>
-  <button class="btn btn-primary reg">Registrarse</button>
 </div>
 
 
