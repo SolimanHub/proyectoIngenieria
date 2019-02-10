0180037fe00c7a25665c015db6bbcd21b729f56a
vbessy10 <vbessy798@yahoo.es>
2019-02-10 13:13:36 -0600

    Cambio menor

--- inventario.php
+++ inventario.php
@@ -39,7 +39,7 @@
 						<li><b> <a href="#">Inventario</a></b></li>
 						<li><b> <a href="catalogo.php">Catalogo De Servicios</a></b></li>
 						<li><b> <a href="resultados.php">Emision De Resultados</a></li>
-						<li><b> <a>Cliente</a></b></li>
+						<li><b> <a href="cliente.php">Cliente</a></b></li>
 						<li><b> <a href="administracion.php">Administracion</a></b></li>
 				</ul>
 				<!--Final De La Lista Anidada-->
