3aab89b5240687606ed03ed5b8d75c2652081506
vbessy10 <vbessy798@yahoo.es>
2019-02-10 13:11:08 -0600

    Cambio menor

--- catalogo.php
+++ catalogo.php
@@ -41,7 +41,7 @@
 					<li><b> <a href="inventario.php">Inventario</a></b></li>
 					<li><b> <a href="#">Catalogo De Servicios</a></b></li>
 					<li><b> <a href="resultados.php">Emision De Resultados</a></li>
-					<li><b> <a>Cliente</a></b></li>
+					<li><b> <a href="cliente.php">Cliente</a></b></li>
 					<li><b> <a href="administracion.php">Administracion</a></b></li>
 			</ul>
 			<!--Final De La Lista Anidada-->
